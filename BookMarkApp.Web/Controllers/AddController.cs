558e5611e14fbbbbceda47f4389e76649302ec54
opielanaruto <opielanaruto@gmail.com>
2014-10-15 09:53:33 -0500

    Full CRUD with routing works.

---
+++ BookMarkApp.Web/Controllers/AddController.cs
@@ -0,0 +1,37 @@
+﻿using BookMarkApp.DataModels;
+using BookMarkApp.Db.Adapters.Data;
+using BookMarkApp.Db.Adapters.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookMarkApp.Web.Controllers
+{
+	public class AddController : Controller
+	{
+		IBookMarkAppAdapter _adapter;
+		public AddController()
+		{
+			_adapter = new BookMarkAppAdapter();
+		}
+		public AddController(IBookMarkAppAdapter adapter)
+		{
+			_adapter = adapter;
+		}
+		[HttpGet]
+		public ActionResult AddCategory()
+		{
+			return View();
+		}
+		[HttpPost]
+		public ActionResult AddCategory(string title)
+		{
+			Category category = new Category();
+			category.Title = title;
+			category = _adapter.AddCategory(category);
+			return Redirect("/Home/Index");
+		}
+	}
+}
