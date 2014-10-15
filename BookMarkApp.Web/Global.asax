6a14c0cbf5f05a947a430973c7bcc23d361b3c75
opielanaruto <opielanaruto@gmail.com>
2014-10-14 21:01:02 -0500

    Initial with Identity

---
+++ BookMarkApp.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookMarkApp.Web.MvcApplication" Language="C#" %>
---
+++ BookMarkApp.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookMarkApp.Web.Startup))]
+namespace BookMarkApp.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
